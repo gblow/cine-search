c05a97ad5d8a159464335801acc6bcced733c1a9
KyleGru <136273960+KyleGru@users.noreply.github.com>
2023-12-04 15:20:31 -0500

    created a fuction to fetch genre data from watchmode API and have it successfully display in the console when a genre button is clicked.

--- index.js
+++ index.js
@@ -1,56 +1,102 @@
-const wikiAPIKey = "6e5f803bd59e151c8d9173f058396cb9";
-const wmAPIKey = "cokcLMHE2H1fuhy7JrUfLRhE81oeqANAcPdOEOzp";
-var mainPage = document.getElementById("main-page");
-var genrePage = document.getElementById("genre-page");
-var movieSel = document.getElementById("movie-btn");
-var tvSel = document.getElementById("tv-btn");
-var action = document.getElementById("action-btn");
-var horror = document.getElementById("horror-btn");
-var scifi = document.getElementById("scifi-btn");
-var rom = document.getElementById("rom-btn");
-var com = document.getElementById("com-btn");
-var kids = document.getElementById("kids-btn");
+var mainPage = document.querySelector('.main-page');
+var mainPageBtn0 = document.getElementById('movie-btn'); 
+var mainPageBtn1 = document.getElementById('tv-btn'); 
+var genrePage = document.querySelector('.genre-page');
+var streamingPage = document.querySelector('.streaming-page');
+var resultsPage = document.querySelector('.results-page');
+var prevResultsPageBtn = document.getElementById('pre-results-btn');
+const WatchmodeAPI = "cokcLMHE2H1fuhy7JrUfLRhE81oeqANAcPdOEOzp"
+const WikiAPI = "6e5f803bd59e151c8d9173f058396cb9"
 
-// Sends the user to the genre page
-movieSel.addEventListener("click", function() {
+var genrePageBtn0 = document.getElementById("action-btn");
+var genrePageBtn1 = document.getElementById("horror-btn");
+var genrePageBtn2 = document.getElementById("scifi-btn");
+var genrePageBtn3 = document.getElementById("rom-btn");
+var genrePageBtn4 = document.getElementById("com-btn");
+var genrePageBtn5 = document.getElementById("kids-btn");
+
+
+var streamingPageBtn0 = document.getElementById('netflix-btn'); 
+var streamingPageBtn1 = document.getElementById('max-btn'); 
+var streamingPageBtn2 = document.getElementById('hulu-btn'); 
+var streamingPageBtn3 = document.getElementById('amazon-btn'); 
+var streamingPageBtn4 = document.getElementById('vudu-btn'); 
+var streamingPageBtn5 = document.getElementById('paramount-btn'); 
+var streamingPageBtn6 = document.getElementById('apple-btn'); 
+var streamingPageBtn7 = document.getElementById('disney-btn'); 
+var streamingPageBtn8 = document.getElementById('peacock-btn'); 
+
+
+
+function goToGenrePage() {
     mainPage.style.display = "none";
     genrePage.style.display = "block";
-})
+}
+mainPageBtn0.addEventListener("click", goToGenrePage);
+mainPageBtn1.addEventListener("click", goToGenrePage);
 
-tvSel.addEventListener("click", function() {
+
+function goToStreamingPage() {
     mainPage.style.display = "none";
-    genrePage.style.display = "block";
-});
-
-function makeAPICallAndNav(genre) {
-    var wmURL = `'https://api.watchmode.com/v1/sources/?apiKey=cokcLMHE2H1fuhy7JrUfLRhE81oeqANAcPdOEOzp'`;
-
-    fetch(wmURL)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error("Error", error);
-        })
-        window.location.href = "#streaming-page";
+    genrePage.style.display = "none";
+    streamingPage.style.display = "block";
 }
 
-action.addEventListener("click", function() {
-    makeAPICallAndNav("Action");
-})
-horror.addEventListener("click", function() {
-    makeAPICallAndNav("Horror");
-})
-scifi.addEventListener("click", function() {
-    makeAPICallAndNav("Scifi");
-})
-rom.addEventListener("click", function() {
-    makeAPICallAndNav("Romance");
-})
-com.addEventListener("click", function() {
-    makeAPICallAndNav("Comedy");
-})
-kids.addEventListener("click", function() {
-    makeAPICallAndNav("Kids/Animation");
-})
+function fetchDataByGenre(genre) {
+    var apiUrl = 'https://api.watchmode.com/v1/genres/?apiKey=cokcLMHE2H1fuhy7JrUfLRhE81oeqANAcPdOEOzp'
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(`data for ${genre}:`, data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        })
+}
+
+//Genre page function and click listener
+genrePageBtn0.addEventListener("click", function(){
+    goToStreamingPage();
+    fetchDataByGenre("Action")
+});
+genrePageBtn1.addEventListener("click", function(){
+    goToStreamingPage()
+    fetchDataByGenre("Horror")
+});
+genrePageBtn2.addEventListener("click", function(){
+    goToStreamingPage()
+    fetchDataByGenre("SciFi/Fantasy")
+});
+genrePageBtn3.addEventListener("click", function(){
+    goToStreamingPage()
+    fetchDataByGenre("Romance")
+});
+genrePageBtn4.addEventListener("click", function(){
+    goToStreamingPage()
+    fetchDataByGenre("Comedy")
+});
+genrePageBtn5.addEventListener("click", function(){
+    goToStreamingPage()
+    fetchDataByGenre("Kids/Animation")
+});
+
+
+//Streaming page function and click listener
+
+function goToResultsPage() {
+    mainPage.style.display = "none";
+    genrePage.style.display = "none";
+    streamingPage.style.display = "none";
+    resultsPage.style.display = "block";
+}
+
+streamingPageBtn0.addEventListener("click", goToResultsPage);
+streamingPageBtn1.addEventListener("click", goToResultsPage);
+streamingPageBtn2.addEventListener("click", goToResultsPage);
+streamingPageBtn3.addEventListener("click", goToResultsPage);
+streamingPageBtn4.addEventListener("click", goToResultsPage);
+streamingPageBtn5.addEventListener("click", goToResultsPage);
+streamingPageBtn6.addEventListener("click", goToResultsPage);
+streamingPageBtn7.addEventListener("click", goToResultsPage);
+streamingPageBtn8.addEventListener("click", goToResultsPage);
